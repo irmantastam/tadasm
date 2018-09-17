3c1ec3b093b4ed8ba63a2cec20e098eade8ca4ab
Irmantas Tamasauskas <irmantas.tam@gmail.com>
2018-09-17 12:41:39 +0300

    tadas-physio google verification code

--- src/header.php
+++ src/header.php
@@ -2,6 +2,7 @@
 <html <?php language_attributes(); ?>>
   <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name="google-site-verification" content="f2b5hyRFMnivumtMVMrU_iyjgUJN-MMsN67yxiOkKo4" />
     <title><?php bloginfo('name'); ?> | <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
     <link href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.jpg" rel="shortcut icon">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
