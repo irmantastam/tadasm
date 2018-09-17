1700902c4f61b2f44eabc5224e2d96aff83abb5e
Irmantas Tamasauskas <irmantas.tam@gmail.com>
2018-09-17 17:31:57 +0300

    tadas-physio social icons to the header

--- src/header.php
+++ src/header.php
@@ -19,6 +19,10 @@
           </a>
         </div>
         <nav class="nav" role="navigation">
+          <div class="social-links">
+            <a class="social-links__link social-links__link--instagram" href="https://www.instagram.com/tadas.physio/" target="_blank"/></a>
+            <a class="social-links__link social-links__link--facebook" href="https://www.facebook.com/Tadas.physio" target="_blank"/></a>
+          </div>
           <?php wp_nav_menu() ?>
         </nav>
       </header>
