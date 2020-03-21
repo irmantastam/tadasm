c1faddc7b31a039381719b02e932e38ac75136ff
Irmantas Tamasauskas <irmantas.tam@gmail.com>
2020-03-21 20:31:06 +0200

    tadasm add facebook link

--- src/footer.php
+++ src/footer.php
@@ -1,9 +1,10 @@
     <footer class="footer">
       <div class="footer__text">
-        <?php _e('tadasm.lt 2019', 'es-blank'); ?>
+        <?php _e('tadasm.lt 2020', 'es-blank'); ?>
       </div>
       <div class="social-links">
         <a class="social-links__link social-links__link--instagram" href="https://www.instagram.com/tadas.mikelionis" target="_blank"/></a>
+        <a class="social-links__link social-links__link--facebook" href="https://www.facebook.com/tadas.kinezi/" target="_blank"/></a>
       </div>
     </footer>
     </div>
--- src/header.php
+++ src/header.php
@@ -30,6 +30,7 @@
           </nav>
           <div class="social-links">
             <a class="social-links__link social-links__link--instagram" href="https://www.instagram.com/tadas.mikelionis" target="_blank"/></a>
+            <a class="social-links__link social-links__link--facebook" href="https://www.facebook.com/tadas.kinezi/" target="_blank"/></a>
           </div>
         </div>
       </header>
