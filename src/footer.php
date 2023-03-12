b6675f503ed874213ada28e3b894f278caa56a15
Irmantas Tamašauskas <irmantas.tam@gmail.com>
2023-03-12 16:05:57 +0200

    Update header and footer

--- src/footer.php
+++ src/footer.php
@@ -1,11 +1,13 @@
     <footer class="footer">
-      <div class="footer__text">
-        <?php _e('tadasm.lt 2021', 'tadasm'); ?>
-      </div>
-      <div class="social-links">
-        <a class="social-links__link social-links__link--instagram" href="https://www.instagram.com/innovamed.lt" target="_blank"/></a>
-        <a class="social-links__link social-links__link--facebook" href="https://www.facebook.com/innovamed.lt" target="_blank"/></a>
-      </div>
+      <section class="inner">
+        <div class="footer__text">
+          <?php _e('tadasm.lt', 'tadasm'); echo ' ' . date("Y"); ?>
+        </div>
+        <div class="social-links">
+          <a class="social-links__link social-links__link--instagram" href="https://www.instagram.com/innovamed.lt" target="_blank"/></a>
+          <a class="social-links__link social-links__link--facebook" href="https://www.facebook.com/innovamed.lt" target="_blank"/></a>
+        </div>
+      </section>
     </footer>
     </div>
     <?php wp_footer(); ?>
